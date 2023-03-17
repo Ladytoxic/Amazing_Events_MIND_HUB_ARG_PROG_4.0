79682ace0f09452e4bc6744f01e3e3fa711b7a2a
Almendra Romina R <Almendraromina1@gmail.com>
2023-03-17 03:49:10 -0300

    se agrego datos en la pagina stats

---
+++ js/stats.js
@@ -0,0 +1,101 @@
+let dataEvents;
+let eventos_pasados;
+let eventos_proximos;
+
+const container = document.querySelector('.container-fluid');
+const loader = container.querySelector('.loader');
+
+const events_statistics = document.getElementById('events_statistics');
+const upcoming_events = document.getElementById('upcoming_events');
+const past_events = document.getElementById('past_events');
+
+
+fetch('https://mindhub-xj03.onrender.com/api/amazing')
+    .then(response => response.json())
+    .then(data => {
+        dataEvents = data;
+        eventos_pasados = buscarEventosPasados(dataEvents);
+        eventos_proximos = buscarEventosProximos(dataEvents)
+        pintarEventsTable(eventos_pasados);
+        // loader.style.display = 'none';
+    })
+    .catch(error => console.log(error));
+
+// loader.style.display = 'block';
+
+function buscarEventosPasados(data) {
+    const eventos_past = data.events.filter(evento => evento.date < data.currentDate);
+    eventos_past.sort((a, b) => b.assistance - a.assistance);
+
+    const eventos_pasados_con_porcentaje = eventos_past.map(evento => {
+        const attendance_percentage = Math.round((evento.assistance / evento.capacity) * 100).toFixed(2);
+        const revenues = Math.round(evento.price * evento.assistance);
+        return { ...evento, attendance_percentage, revenues };
+    });
+    return eventos_pasados_con_porcentaje;
+}
+
+function buscarEventosProximos(data) {
+    const eventos_prox = data.events.filter(evento => evento.date >= data.currentDate);
+    eventos_prox.sort((a, b) => b.estimate - a.estimate);
+    return eventos_prox;
+
+}
+
+function pintarEventsTable(evento) {
+    const dos_mayores_assistance = evento.slice(0, 2);
+    const evento_capacity = evento.reduce((event1, event2) => {
+        if (event1.capacity > event2.capacity) {
+            return event1;
+        } else {
+            return event2;
+        }
+    });
+
+    events_statistics.innerHTML = `
+    <tr>
+        <td>${dos_mayores_assistance[0].name} (${dos_mayores_assistance[0].attendance_percentage}%)</td>
+        <td>${dos_mayores_assistance[1].name} (${dos_mayores_assistance[1].attendance_percentage}%)</td>
+        <td>${evento_capacity.name} (${evento_capacity.capacity})</td>
+    </tr>`;
+    statisticsByCategory(eventos_pasados, past_events);
+    statisticsByCategory(eventos_proximos, upcoming_events);
+}
+
+function eventsCategory(events) {
+    return events.reduce((acc, event) => {
+        if (!acc[event.category]) {
+            acc[event.category] = [];
+        }
+        acc[event.category].push(event);
+        return acc;
+    }, {});
+}
+
+function statisticsByCategory(events, tabla) {
+    const eventsByCategory = eventsCategory(events);
+    const statisticsByCategory = Object.keys(eventsByCategory).map(category => {
+        const categoryEvents = eventsByCategory[category];
+        const totalCapacity = categoryEvents.reduce((acc, event) => acc + event.capacity, 0);
+        const totalAssistance = categoryEvents.reduce((acc, event) => acc + (event.assistance || event.estimate), 0);
+        const attendancePercentage = Math.round((totalAssistance / totalCapacity) * 100).toFixed(2);
+        const totalRevenues = categoryEvents.reduce((acc, event) => acc + (event.price * (event.assistance || event.estimate)), 0);
+
+        return {
+            category,
+            attendancePercentage,
+            totalRevenues
+        };
+    });
+    statisticsByCategory.forEach((evento) => {
+        tabla.innerHTML += `
+        <tr>
+        <td>${evento.category}</td>
+            <td>${evento.totalRevenues.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+            <td>(${evento.attendancePercentage}%)</td>
+        </tr>
+        `;
+    });
+    return statisticsByCategory;
+}
+
